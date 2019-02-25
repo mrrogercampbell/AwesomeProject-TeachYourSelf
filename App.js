e10d77f61f47bb7c080e05b5368125b3f5d55bc4
Roger Campbell II <rc1336@gmail.com>
2019-02-25 11:36:07 -0500

    Copied and pasted some basic components from the React Native walkthrough into my proect. Components: Bananas, Blink, and Greeting.

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Bananas from './Components/Bananas/Bananas'
+import Greeting from './Components/Greeting/Greeting'
 
 export default class HelloWorldApp extends Component {
   render() {
@@ -7,7 +9,10 @@ export default class HelloWorldApp extends Component {
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello world!</Text>
         <Text>YASS BECKY!!</Text>
+        <Bananas />
+        <Greeting />
       </View>
+
     );
   }
 }
